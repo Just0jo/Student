ab20781ca0c5772fde7c6a1ce64b4af9af87674e
Praise Ojofeitimi <G00407342@atu.ie>
2022-10-27 20:54:52 +0100

    Ask for student name

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -1,5 +1,14 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class StudentApp {
+    public static void main(String[] args) {
+        System.out.println("Please enter Student name:");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("You Entered : "+ name);
+
+    }
 
 }
